26191dd3d441864fab4a7a9f3a3fec2984b0da9d
Ayodeji0955 <54115645+Ayodeji0955@users.noreply.github.com>
2020-06-04 18:31:32 +0100

    hng.05638.js
    This is part of the code.

---
+++ hng.05638.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>My first stage code</h2>
+<p>I used javascript for coding.</p>
+<p>This is part of the code.</p>
+
+
+
+</body>
+</html> 
+
+ <script>
+console.log("Hello World this is olugbade ayodeji with HNG05638 ID using javascript for stage 2 task");
+ 
+</script>
+
+		
+</body>
+
+</html>
+
